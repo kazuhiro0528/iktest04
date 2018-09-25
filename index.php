b8cf1b50410da64ed79abaaaebc7e7378a239b68
kazuhiro0528 <43464932+kazuhiro0528@users.noreply.github.com>
2018-09-25 11:47:27 +0900

    test
    test

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?
 echo 'phpのテストです。<br>';
+echo 'phpのテストです。<br>';
 $dbinfo = parse_url(getenv('DATABASE_URL'));
 $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
